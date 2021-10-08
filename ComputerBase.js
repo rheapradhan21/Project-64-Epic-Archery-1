46ecd6d93fdbc6a368994948d35cc4315d559c94
rheapradhan21 <87064604+rheapradhan21@users.noreply.github.com>
2021-10-08 12:10:13 +0300

    Add files via upload

---
+++ ComputerBase.js
@@ -0,0 +1,33 @@
+class computerBase{
+    constructor(x,y,w,h){
+        this.w=w;
+        this.h=h;
+
+       var options={
+           isStatic = true
+        };
+
+        this.body=Bodies.rectangle(x,y,w,h,options);
+        this.image = loadImage("./assets/base2.png");
+        World.add(world,this.body); 
+
+
+    }
+
+    display(){
+
+        var pos= this.body.position;
+        var angle= this.body.angle;
+
+        push()
+        translate(pos.x,pos.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image,0,0,this.w,this.h);
+        pop();
+
+
+    }
+  
+   
+}
